c896ee8a676debfca67ea2c879b4ab392ec113a1
Copser <iamrmot@gmail.com>
2015-11-03 21:17:52 +0100

    Napisan inicijalni test za blog/models.py

--- poets_blog/blog/tests.py
+++ poets_blog/blog/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from models import Post
+
 
 # Create your tests here.
+class PostTests(TestCase):
+
+    """Docstring for PostTests. """
+    def test_str(self):
+        my_title = Post(
+            title='Ovo je blog Profesora i pesnika Pere Skovo-a')
+        self.assertEquals(
+            str(my_title),
+            'Ovo je blog Profesora i pesnika Pere Skovo-a',
+        )
