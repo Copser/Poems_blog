27234268224c319514f26abf26624e22a6642943
Copser <iamrmot@gmail.com>
2015-11-03 21:41:28 +0100

    Sredjivanje admina

--- poets_blog/blog/admin.py
+++ poets_blog/blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from blog.models import Post
 
+
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    """Docstring for PostAdmin. """
+    list_display = ('title', 'created_at')
+
+admin.site.register(Post, PostAdmin)
