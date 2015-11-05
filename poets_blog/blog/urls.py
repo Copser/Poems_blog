76c4d720255a8ad7be98379fd72466ec86b5e8c6
Copser <iamrmot@gmail.com>
2015-11-05 21:48:26 +0100

    Reseni url za slug

--- poets_blog/blog/urls.py
+++ poets_blog/blog/urls.py
@@ -3,5 +3,5 @@ from blog import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<post_url>\w+)/$', views.post, name='post'),
+    url(r'^(?P<slug>[\w|\-]+)/$', views.post, name='post'),
 ]
