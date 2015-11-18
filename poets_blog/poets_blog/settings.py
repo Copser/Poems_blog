530b97520162e581635b23f8f818a2fff3bf862d
Copser <iamrmot@gmail.com>
2015-11-18 20:50:45 +0100

    Sredjujem DATABASE u settings.py

--- poets_blog/poets_blog/settings.py
+++ poets_blog/poets_blog/settings.py
@@ -81,11 +81,6 @@ WSGI_APPLICATION = 'poets_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
     }
 }
 
