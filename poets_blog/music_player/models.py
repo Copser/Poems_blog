d6019dbc5d6e112626fc9e8585e702641d8b0a32
Copser <iamrmot@gmail.com>
2015-12-09 22:52:48 +0100

    Napravljen model za music_app

--- poets_blog/music_player/models.py
+++ poets_blog/music_player/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Song(models.Model):
+
+    """Docstring for Song. Ovaj model ce sadrzati dva modela ime pesme,
+       i FileField za hendlovanje pesmama.
+       This app will contain two model fields, one name of the tracks,
+       and FileField to handle are songs."""
+    song_name = models.CharField(max_length=128)
+    audio_file = models.FileField()
